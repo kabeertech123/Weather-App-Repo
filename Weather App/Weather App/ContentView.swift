8ea60b1ad1aa755eef1db4c9b92e24780202abcd
Kabeer Warsi <kwarsi541@gmail.com>
2026-05-04 21:17:21 +0100

    Basic layout
    Just created the stacks needed
    Created a cool background
    
    :)

---
+++ Weather App/Weather App/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  Weather App
+//
+//  Created by Kabeer Warsi on 04/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        ZStack{// contains all elements
+            LinearGradient(
+                gradient: Gradient(colors: [ .blue,  .white]),
+                startPoint: .topLeading ,
+                endPoint: .bottomTrailing
+            )
+            .edgesIgnoringSafeArea(.all)
+            
+            
+            VStack{
+                
+                
+                
+                VStack{
+                    // City name
+                }
+                
+                VStack{
+                    //icon
+                    
+                    //displays temp
+                }
+                
+                HStack{ //contains details of day, icons and temps
+                    
+                    VStack{ // Day, icon and temp all aligned vertically (will be repeated 6x)
+                        
+                    }
+                }
+                
+                
+                HStack{// button that changes time of day
+                    
+                }
+            }
+        }
+        
+       
+    }
+}
+
+#Preview {
+    ContentView()
+}
