a32268ea90a558bf845ce1263a4ee91fcc9af3c9
Kabeer Warsi <kwarsi541@gmail.com>
2026-05-05 21:59:13 +0100

    added the city name

--- Weather App/Weather App/ContentView.swift
+++ Weather App/Weather App/ContentView.swift
@@ -20,7 +20,7 @@ struct ContentView: View {
             
             
             VStack{
-                
+                Text("Cupertino, CA")
                 
                 
                 VStack{
